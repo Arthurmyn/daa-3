a1d78fdbc165ee252b68de630797806372774f2b
Arthur <arthurrr4ik1@icloud.com>
2025-10-24 12:42:40 +0500

    Add MST assignment with Prim and Kruskal algorithms

---
+++ src/main/java/algorithms/PrimMST.java
@@ -0,0 +1,13 @@
+package algorithms;
+
+/**
+ * Hello world!
+ *
+ */
+public class PrimMST
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
