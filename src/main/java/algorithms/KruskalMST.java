0af062965e3416ed5c5bfddec8738830cb4e3318
Arthur <arthurrr4ik1@icloud.com>
2025-10-26 16:40:17 +0500

    Updated json file

--- src/main/java/algorithms/KruskalMST.java
+++ src/main/java/algorithms/KruskalMST.java
@@ -1,17 +1,5 @@
 package algorithms;
 
 public class KruskalMST {
-    private int[] parent;
-    private int[] rank;
-    private int[] sz;
-    public KruskalMST(int N){
-        parent = new int[N];
-        rank = new int[N];
-        sz = new int[N];
-        for(int i = 0; i < N; i++){
-            parent[i] = i;
-            sz[i] = 1;
-        }
-    }
 
 }
--- src/main/java/algorithms/PrimMST.java
+++ src/main/java/algorithms/PrimMST.java
@@ -1,13 +1,5 @@
 package algorithms;
 
-/**
- * Hello world!
- *
- */
-public class PrimMST
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class PrimMST {
+
 }
